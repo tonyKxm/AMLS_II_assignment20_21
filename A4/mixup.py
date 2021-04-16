548368a8f100697d14160000a849a5af1f22f361
tonyKxm <75300166+tonyKxm@users.noreply.github.com>
2021-04-16 16:43:52 +0800

    Update mixup.py

--- A4/mixup.py
+++ A4/mixup.py
@@ -4,7 +4,7 @@ import keras
 import numpy as np
 import pandas as pd
 from keras.preprocessing.image import ImageDataGenerator
-from mixup_generator import MixupImageDataGenerator
+from A4.mixup_generator import MixupImageDataGenerator
 
 
 # setup current work path
@@ -22,22 +22,18 @@ img_gen = ImageDataGenerator(rescale = 1.0/255.0, horizontal_flip = True, vertic
 def preProcessing():
     # read image and label csv file of train, valid, test set
     train = os.path.join(baseDir ,'Datasets','train_img.csv')
-    valid = os.path.join(baseDir ,'Datasets','valid_img.csv')
     test = os.path.join(baseDir ,'Datasets','test_img.csv')
     train_img=pd.read_csv(train)
-    valid_img=pd.read_csv(valid)
     test_img=pd.read_csv(test)
     
     train_img.drop(columns=['Unnamed: 0'],inplace=True)
-    valid_img.drop(columns=['Unnamed: 0'],inplace=True)
     test_img.drop(columns=['Unnamed: 0'],inplace=True)
     
     # map string type labels to int type
     real_labels = {'cbb':0,'cbsd':1,'cgm':2,'cmd':3,'healthy':4}
     train_img['class_num'] = train_img['label'].map(real_labels)
-    valid_img['class_num'] = valid_img['label'].map(real_labels)
     test_img['class_num'] = test_img['label'].map(real_labels)
-    return train_img,valid_img,test_img
+    return train_img,test_img
 
 
 def train(train_img):
@@ -47,13 +43,6 @@ def train(train_img):
     # evaluate_generator will return loss and acc of the prediction
     loss,acc = final_model.evaluate_generator(train,steps=train.n//12)
     return acc
-
-def valid(valid_img):
-    valid = img_gen.flow_from_dataframe(valid_img, directory = img,x_col = 'filename', y_col = 'label', 
-                                  target_size =(220, 220), class_mode = 'categorical', shuffle = False,
-                                  batch_size = 12, color_mode = 'rgb')
-    loss,acc = final_model.evaluate_generator(valid,steps=valid.n//12)
-    return acc  
     
 def test(test_img):
     test = img_gen.flow_from_dataframe(test_img, directory = img,x_col = 'filename', y_col = 'label', 
