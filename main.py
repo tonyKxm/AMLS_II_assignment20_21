f8d81b870d87618eac33cdd7d51aa5dace575d3e
tonyKxm <75300166+tonyKxm@users.noreply.github.com>
2021-04-18 20:48:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ warnings.filterwarnings('ignore')
 from A4 import mixup as a4
 # ======================================================================================================================
 # Data preprocessing
+print('This process is slow, please wait for at least 4~5 minutes')
 train,test = a4.preProcessing()
 # # Task A4
 acc_train = a4.train(train) 
